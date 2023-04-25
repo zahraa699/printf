c7d277332bb399d75c6e13350002dbd57a1acb33
khaledmasry0 <khaledmohamed3360@gmail.com>
2023-04-25 12:48:55 -0700

    add params.c file

---
+++ params.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * init_params - clears struct fields and reset
+ * @params: params struct
+ * @ap: the arg pointer
+ * Return: void
+*/
+
+void init_params(params_t *params, va_list ap)
+{
+	params->unsign = 0;
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
+	params->width = 0;
+	params->precision = UINT_MAX;
+	params->h_modifier = 0;
+	params->l_modifier = 0;
+	(void)ap;
+}
