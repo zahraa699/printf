d00bd6142368374457bff71213a8df93e381ff6f
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-25 17:45:15 -0700

    fix & updated main.h

--- main.h
+++ main.h
@@ -11,7 +11,7 @@
 
 #define NULL_STRING "(null)"
 
-#define PARAMS_INIT (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 #define CONVERT_LOWERCASE 1
 #define CONVERT_UNSIGNED 2
