f964be77458a41103a9e8a43d76d1228619177b8
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-25 17:30:46 -0700

    update main.h

--- main.h
+++ main.h
@@ -17,6 +17,40 @@
 #define CONVERT_UNSIGNED 2
 
 /**
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag if unsigned value
+ *
+ * @plus_flag: on if plus_flag specified
+ * @space_flag: on if hashtag_flag specified
+ * @hashtag_flag: on if _flag specified
+ * @zero_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
+ *
+ * @width: field width specified
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+typedef struct parameters
+{
+	unsigned int unsign		: 1;
+	unsigned int plus_flag		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int minus_flag		: 1;
+
+	unsigned int width;
+	unsigned int precision;
+
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
+} params_t;
+
+/**
  * struct specifier - this function is struct token
  * @specifier: this is format token
  * @f: this is associtated function
